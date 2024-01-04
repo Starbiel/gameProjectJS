24c2e31627440c66f130eb94321387016104f2a1
Wesley Parastchuk <wesleyparastchuk13@gmail.com>
2024-01-04 18:08:15 -0300

    difficult change

--- gameFunctions/settingGame.js
+++ gameFunctions/settingGame.js
@@ -1,67 +1,47 @@
 import { lastDirectionFix } from "../objectSets/playerSet.js";
 import { personOne } from "../script.js";
+import { difficults } from "../constants/difficults.js";
+
 class Config {
-    //vw and vh
-    //gameArea = [80,50];
-    constructor(difficult, area) {
-        if(difficult == 'normal') {
-            this.playerSpeed = 5;
-            this.moneyValue = 50;
-            this.speedSpawn = 3500;
-            this.enemyLife = 100;
-            this.enemySpeed = 3;
-            this.speedEnemyShoot = 5;
-            this.shooterEnemy = 55;
-        }
-        else if(difficult == 'easy') {
-            this.playerSpeed = 7;
-            this.moneyValue = 100;
-            this.speedSpawn = 4000; //ms
-            this.enemyLife = 80;
-            this.enemySpeed = 3;
-            this.speedEnemyShoot = 3;
-            this.shooterEnemy = 40;
-        }
-        else {
-            this.playerSpeed = 2;
-            this.moneyValue = 320;
-            this.speedSpawn = 4000;
-            this.enemyLife = 150;
-            this.enemySpeed = 1;
-            this.speedEnemyShoot = 7;
-            this.shooterEnemy = 70;
-        }
-        this.gameArea = area;
-    }
+	//vw and vh
+	//gameArea = [80,50];
+	constructor(difficult, area) {
+		if (!difficults[difficult]) 
+            throw new Error("Difficult not found");
+		this.difficult = difficults[difficult];
+		Object.entries(this.difficult).forEach(([key, value]) => {
+			this[key] = value;
+		});
+		this.gameArea = area;
+	}
 
-    createArea() {
-        const area = document.querySelector('#container');
-        area.style.width = this.gameArea[0] + 'vw';
-        area.style.height = this.gameArea[1] + 'vh';
-        this.containerWidth = area.offsetWidth;
-        this.containerHeight = area.offsetHeight;
-        this.containerAll = area.getBoundingClientRect();
-        this.containerElement = area;
-        const gunAmmo = document.querySelector('#ammo');
-        gunAmmo.style.top = this.containerHeight + 'px';
-    }
-
+	createArea() {
+		const area = document.querySelector("#container");
+		area.style.width = this.gameArea[0] + "vw";
+		area.style.height = this.gameArea[1] + "vh";
+		this.containerWidth = area.offsetWidth;
+		this.containerHeight = area.offsetHeight;
+		this.containerAll = area.getBoundingClientRect();
+		this.containerElement = area;
+		const gunAmmo = document.querySelector("#ammo");
+		gunAmmo.style.top = this.containerHeight + "px";
+	}
 }
 
-export let ConfigReady = new Config('hard', [80,60]);
-
+export let ConfigReady = new Config("hard", [80, 60]);
 
 //arg function are html elements(obj);
 
 export function crossoverTester(obj1, obj2) {
+    const ret1 = obj1.getBoundingClientRect();
+    const ret2 = obj2.getBoundingClientRect();
     return (
-        obj1.getBoundingClientRect().x < obj2.getBoundingClientRect().x + obj2.getBoundingClientRect().width &&
-        obj1.getBoundingClientRect().x + obj1.getBoundingClientRect().width  > obj2.getBoundingClientRect().x &&
-        obj1.getBoundingClientRect().y < obj2.getBoundingClientRect().y + obj2.getBoundingClientRect().height &&
-        obj1.getBoundingClientRect().y + obj1.getBoundingClientRect().height > obj2.getBoundingClientRect().y
+        ret1.left < ret2.left + ret2.width &&
+        ret1.left + ret1.width > ret2.left &&
+        ret1.top < ret2.top + ret2.height &&
+        ret1.top + ret1.height > ret2.top
     );
 }
-
 
 //Position's enemy
 export function generateCoins(y, x) {
